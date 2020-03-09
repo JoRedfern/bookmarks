da8ccf09a441023932dea01f44ddc4772c08ecbf
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-09 16:40:09 +0000

    connected to db:

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -4,9 +4,9 @@ describe Bookmarks do
   describe '.all' do
     it 'returns all bookmarks' do
       bookmarks = Bookmarks.all
-      expect(bookmarks).to include('http://www.makersacademy.com')
-      expect(bookmarks).to include('http://www.destroyallsoftware.com')
-      expect(bookmarks).to include('http://www.google.com')
+      expect(bookmarks).to include('http://makersacademy.com')
+      expect(bookmarks).to include('http://destroyallsoftware.com')
+      expect(bookmarks).to include('http://google.com')
     end
   end
 end
