50f1cb09e01f89e536da4776871d16371b789e3a
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-11 14:36:36 +0000

    end of ch 9, set up test environment

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -7,8 +7,8 @@ class Bookmark
     connection = PG.connect(dbname: 'bookmark_manager_test')
   else
     connection = PG.connect(dbname: 'bookmark_manager')
-    result = connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| bookmark['url'] }
   end
+  result = connection.exec("SELECT * FROM bookmarks;")
+  result.map { |bookmark| bookmark['url'] }
 end
 end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -4,7 +4,6 @@ describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
-
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://makersacademy.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://destroyallsoftware.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://google.com');")
